c23b0051effcfae5cc7b9c4b3c97b237272ddfc6
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2022-10-05 01:53:06 +0300

    HW2.5 Collection

---
+++ src/main/java/com/homework2/Collection/homework2/Collection/Application.java
@@ -0,0 +1,13 @@
+package com.homework2.Collection.homework2.Collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
